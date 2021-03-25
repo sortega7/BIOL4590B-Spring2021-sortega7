a855305ac60cc3d3551f8f7f523ead952d75aecf
sortega7 <81382205+sortega7@users.noreply.github.com>
2021-03-25 13:47:14 -0400

    Add files via upload

---
+++ exercise1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+echo "Hello World"
+echo "Hello World"
+echo "Hello World"
+echo "Hello World"
+echo "Hello World"
+echo "Hello World"
+echo "Hello World"
+echo "Hello World"
+echo "Hello World"
+echo "Hello World" 
+
+sleep 2m
+
+ls -A |wc -l >exercise1.txt
